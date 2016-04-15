15e3e31e2a7232a91d7b7094fb5e3f928e316fb3
toshiotm <toshi.otm@gmail.com>
2016-04-15 14:18:01 +0900

    fix bug

--- app_template.rb
+++ app_template.rb
@@ -64,7 +64,7 @@ application do
         
         I18n.enforce_available_locales = true
         config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb, yml}').to_s]
-        config.i18n.default_local = :ja
+        config.i18n.default_locale = :ja
         
         config.generators do |g|
             g.template_engine :slim
